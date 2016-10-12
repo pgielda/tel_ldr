71996c85773a05ddd8bbe88b9895c2e084ccadff
Peter Gielda <pgielda@antmicro.com>
2016-10-12 23:43:33 +0200

    wip

--- el.c
+++ el.c
@@ -12,7 +12,7 @@
 #include <sys/mman.h>
 #include <unistd.h>
 #include <stdint.h>
-
+#include <sys/stat.h>
 
 #ifdef __linux__
 #include <execinfo.h>
@@ -325,6 +325,13 @@ int main(int argc, char* argv[]) {
         printf("needed: %s", dstr + *neededp);
 	/* TODO: temporarily we are not loading the libs */
         if (dlopen(dstr + *neededp, RTLD_NOW | RTLD_GLOBAL) == NULL) {
+		if (!strncmp(dstr + *neededp, "libreadline.", 12)) {
+			printf("loading libreadline!\n");
+			if (dlopen("libreadline.dylib", RTLD_NOW | RTLD_GLOBAL) == NULL) printf("failed :( :(\n");
+		}
+		if (!strncmp(dstr + *neededp, "libncurses.", 11)) {
+				if (dlopen("libncurses.dylib", RTLD_NOW | RTLD_GLOBAL) == NULL) printf("failed :( :(\n");
+			}
 		printf(" (not found)\n");
 	} else {
 		printf(" (loaded)\n");
@@ -388,7 +395,7 @@ int main(int argc, char* argv[]) {
 		      } else *addr = *(int*)val;
               } else {
                 fprintf(stderr, "undefined symbol %s\n", sname);
-		abort();
+		//abort();
               }
 	      break;
             }
@@ -443,6 +450,8 @@ int main(int argc, char* argv[]) {
 }
 
 #ifdef __MACH__
+// stuff taken from https://github.com/evanphx/ulysses-libc
+
 char *bindtextdomain(const char *domainname, const char *dirname) {
 	return NULL;
 }
@@ -451,8 +460,33 @@ char *textdomain(const char *domainname) {
 	return NULL;
 }
 
-int32_t **__ctype_tolower_loc() {
-   return NULL;
+static const int32_t table[] = {
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,
+32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,
+48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,
+64,
+'a','b','c','d','e','f','g','h','i','j','k','l','m',
+'n','o','p','q','r','s','t','u','v','w','x','y','z',
+91,92,93,94,95,96,
+'a','b','c','d','e','f','g','h','i','j','k','l','m',
+'n','o','p','q','r','s','t','u','v','w','x','y','z',
+123,124,125,126,127,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+};
+
+static const int32_t *const ptable = table+128;
+
+const int32_t **__ctype_tolower_loc(void)
+{
+	return (void *)&ptable;
 }
 
 int fputs_unlocked(const char *str, FILE *stream) {
@@ -467,4 +501,63 @@ size_t __fpending (FILE *fp) {
 void* __rawmemchr(const void* s, int c) {
 	  return memchr(s, c, -1);
 }
+
+int rl_filename_quoting_desired = 1;
+
+void *rl_last_func = NULL;
+
+int rl_sort_completion_matches = 1;
+int history_max_entries=10;
+
+void *rl_directory_completion_hook = NULL;
+void *rl_filename_rewrite_hook = NULL;
+
+int __xstat64(int version, const char *file, void *buf) {
+   return stat(file, buf);
+}
+
+FILE *fopen64(const char *path, const char *mode) {
+	return fopen(path, mode);
+}
+
+ssize_t __getdelim(char ** linep, size_t *linecap, int delimeter, FILE *stream) {
+	return getdelim(linep, linecap, delimeter, stream);
+}
+
+#define X(x) x
+
+static const unsigned short table2[] = {
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),
+X(0x200),X(0x320),X(0x220),X(0x220),X(0x220),X(0x220),X(0x200),X(0x200),
+X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),
+X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),X(0x200),
+X(0x160),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),
+X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),
+X(0x8d8),X(0x8d8),X(0x8d8),X(0x8d8),X(0x8d8),X(0x8d8),X(0x8d8),X(0x8d8),
+X(0x8d8),X(0x8d8),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),
+X(0x4c0),X(0x8d5),X(0x8d5),X(0x8d5),X(0x8d5),X(0x8d5),X(0x8d5),X(0x8c5),
+X(0x8c5),X(0x8c5),X(0x8c5),X(0x8c5),X(0x8c5),X(0x8c5),X(0x8c5),X(0x8c5),
+X(0x8c5),X(0x8c5),X(0x8c5),X(0x8c5),X(0x8c5),X(0x8c5),X(0x8c5),X(0x8c5),
+X(0x8c5),X(0x8c5),X(0x8c5),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),
+X(0x4c0),X(0x8d6),X(0x8d6),X(0x8d6),X(0x8d6),X(0x8d6),X(0x8d6),X(0x8c6),
+X(0x8c6),X(0x8c6),X(0x8c6),X(0x8c6),X(0x8c6),X(0x8c6),X(0x8c6),X(0x8c6),
+X(0x8c6),X(0x8c6),X(0x8c6),X(0x8c6),X(0x8c6),X(0x8c6),X(0x8c6),X(0x8c6),
+X(0x8c6),X(0x8c6),X(0x8c6),X(0x4c0),X(0x4c0),X(0x4c0),X(0x4c0),X(0x200),
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+};
+
+static const unsigned short *const ptable2 = table2+128;
+
+const unsigned short **__ctype_b_loc(void)
+{
+	return (void *)&ptable2;
+}
+
 #endif
